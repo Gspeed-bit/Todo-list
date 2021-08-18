0d569ab1dbe59a74f2d6ababcebbbbdf1c54aa78
Ayodeji adeyeye <adeyeye17@gmail.com>
2021-08-18 21:13:54 +0200

    todo-list

---
+++ app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUl = document.getElementById('todos');
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    addTodo()
+})
+
+
+
+const addTodo =(todo)=>{
+    let todoText = input.value
+
+    if(todo){
+        todoText = todo.text
+    }
+
+
+  if(todoText){
+     const todoEl = document.createElement('li')
+    if(todo && todo.completed){
+        todoEl.classList.toggle("completed")
+    }
+        todoEl.innerText = todoText
+        todosUl.appendChild(todoEl)
+        input.value=""
+
+    }
+
+}
