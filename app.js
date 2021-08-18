ee9a929190c814bc1ccf2fca9dc9e69e7d32f999
Ayodeji adeyeye <adeyeye17@gmail.com>
2021-08-18 23:13:24 +0200

    complete project

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUl = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+} 
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
     addTodo()
 })
-
-
 
 const addTodo =(todo)=>{
     let todoText = input.value
@@ -21,17 +24,44 @@ const addTodo =(todo)=>{
   if(todoText){
      const todoEl = document.createElement('li')
     if(todo && todo.completed){
-        todoEl.classList.toggle("completed")
+        todoEl.classList.add("completed")
     }
         todoEl.innerText = todoText
 
-        todoEl.addEventListener("click", ()=>
-        todoEl.classList.toggle("completed"))
         
+        todoEl.addEventListener("click", ()=>{
+        todoEl.classList.toggle("completed")
+        updateLS()
+    }); 
+        
+        todoEl.addEventListener('contextmenu', (e) => {
+             e.preventDefault();
+          todoEl.remove()
+          updateLS()
+         });
+
+
         todosUl.appendChild(todoEl)
         
-    input.value=""
+    input.value=''
+
+    updateLS()
 
     }
 
+}
+
+const updateLS = ()=> {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
